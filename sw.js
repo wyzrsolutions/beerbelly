96a1392f9cb3ce70417093b217a16e209cafc70c
Wyzr <157423585+wyzrsolutions@users.noreply.github.com>
2026-01-13 03:50:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ self.addEventListener("push", (event) => {
   const title = data.title || "Ny workout";
   const options = {
     body: data.body || "Någon har lagt upp en ny workout.",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
+    icon: "https://cdn.prod.website-files.com/68e01de2e7d9d52f889935ed/69654f38fc18d3fa51bade1a_EA339E66-CA1B-452C-90AA-2A9E685C%20(1).png",
+    badge: "https://cdn.prod.website-files.com/68e01de2e7d9d52f889935ed/69654f38fc18d3fa51bade1a_EA339E66-CA1B-452C-90AA-2A9E685C%20(1).png",
     data: data.url || "/"
   };
   event.waitUntil(self.registration.showNotification(title, options));
